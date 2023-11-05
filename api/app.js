8c62b85e9b55b40a0ef60bf00d77e23bd0d61375
Kamil Galazka <kamilg332@gmail.com>
2023-11-06 00:13:33 +0100

    add bcrypt for password encrypting

--- api/app.js
+++ api/app.js
@@ -1,17 +1,31 @@
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
 const {Client} = require('pg')
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
 
 const app = express()
 const port = process.env.PORT || 3000
-const client = new Client({
-    user: 'docker',
-    host: 'db',
-    database: 'pregnapp',
-    password: 'postgres',
-    port: 5432,
-})
+const saltRounds = 10
+let client
+
+if (process.env.NODE_ENV === 'production') {
+    client = new Client({
+        user: 'docker',
+        host: 'db',
+        database: 'pregnapp',
+        password: 'postgres',
+        port: 5432,
+    })
+} else {
+    client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'pregnapp',
+        password: 'kamil',
+        port: 5433,
+    })
+}
 
 app.use(cors({origin: '*'}))
 
@@ -19,15 +33,15 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 client.connect()
-    .then(() => console.log('Połączono z bazą danych PostgreSQL'))
-    .catch(err => console.error('Błąd podczas łączenia z bazą danych:', err));
+    .then(() => console.log('Connected to database'))
+    .catch(err => console.error('Connection to database failed:', err))
 
 process.on('exit', () => {
-    client.end();
+    client.end()
 })
 
 app.listen(port, () => {
-    console.log('test2')
+    console.log('Server started...')
 })
 
 app.post('/api/user/create', (req, res) => {
@@ -38,24 +52,58 @@ app.post('/api/user/create', (req, res) => {
         password,
     } = req.body
 
-    client.query(`insert into users (name, lastname, email, password_hash, role, verification_token,
-                                     reset_password_token)
-                  values ('${name}', '${lastname}', '${email}', '${password}', 'user', 't1', 't2')`, (err, result) => {
-        if (err) {
-            console.error('Błąd podczas zapytania do bazy danych:', err);
-            res.status(500).send(err.detail);
-        } else {
-            res.status(200).json({
-                status: 'User created',
-            });
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (hash) {
+            client.query(`insert into users (name, lastname, email, password_hash, role, verification_token,
+                                             reset_password_token)
+                          values ('${name}', '${lastname}', '${email}', '${hash}', 'user', 't1',
+                                  't2')`, (err, result) => {
+                if (err) {
+                    console.error('Błąd podczas zapytania do bazy danych:', err);
+                    res.status(500).send(err.detail);
+                } else {
+                    res.status(200).json({
+                        status: 'User created',
+                    });
+                }
+            })
         }
     })
-
 })
 
 app.get('/api/user/login', (req, res) => {
-    console.log(req)
-    // client.query(`select ${}`)
+    const {
+        email,
+        password,
+    } = req.body
+
+    client.query(`select email
+                  from users
+                  where email = '${email}'`, (err, result) => {
+        if (err || !result.rowCount) {
+            res.status(500).json({
+                status: 'Wrong password or email'
+            })
+        }
+    })
+
+    client.query(`select password_hash
+                  from users
+                  where email = '${email}'`, (err, result) => {
+        const hashedPassword = result.rows[0].password_hash
+
+        bcrypt.compare(password, hashedPassword, (err, result) => {
+            if (result) {
+                res.status(200).json({
+                    status: 'Login successful'
+                })
+            } else {
+                res.status(500).json({
+                    status: 'Wrong password or email'
+                })
+            }
+        })
+    })
 })
 
 app.get('/api/navigation', (req, res) => {
