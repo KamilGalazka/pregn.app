39888ef1db33c4230f304f2b56570e2fc5cbb577
Kamil Galazka <kamilg332@gmail.com>
2024-01-14 19:38:30 +0100

    change layout for error page

--- front/src/views/ErrorView.vue
+++ front/src/views/ErrorView.vue
@@ -1,20 +1,23 @@
 <script setup></script>
 
 <template>
-  <div class="error-page col-4">
+  <div class="error-page col-4 d-flex flex-column align-items-center m-auto">
+    <img class="error-page__img" src="../assets/lost-child.svg" />
     <h1 class="error-page__header">{{ $t("error.header404") }}</h1>
-    <p class="error-page__info text-center">{{ $t("error.info404") }}</p>
+    <p class="error-page__info">{{ $t("error.info404") }}</p>
   </div>
 </template>
 
 <style scoped lang="scss">
 .error-page {
-  margin: 120px auto 0;
+  &__img {
+    width: 450px;
+    height: 400px;
+  }
 
   &__header {
+    font-size: 64px;
     border: none;
-    text-align: center;
-    margin-bottom: 24px;
   }
 }
 </style>
