c3fc8dd6b8004489f99ae265b86084f2c5ae6400
Kamil Galazka <kamilg332@gmail.com>
2024-01-16 23:48:06 +0100

    add sorts method for notes, change layout on calendar page

--- front/src/views/CalendarView.vue
+++ front/src/views/CalendarView.vue
@@ -23,9 +23,51 @@ const getUserCalendar = async () => {
     });
 
     savedNotes.value = response.data.response;
+
+    sortNotesByDate();
+    sortNotesByPriority();
   } catch (error) {
     console.log(error);
   }
+};
+
+const sortNotesByPriority = () => {
+  savedNotes.value.sort((a, b) => b.priority - a.priority);
+};
+
+const sortNotesByDate = () => {
+  savedNotes.value.sort((a, b) => {
+    const firstDateParts = a.date.split("/");
+    const secondDateParts = b.date.split("/");
+    const newFirstDate = new Date(
+      firstDateParts[2],
+      firstDateParts[1] - 1,
+      firstDateParts[0]
+    );
+    const newSecondDate = new Date(
+      secondDateParts[2],
+      secondDateParts[1] - 1,
+      secondDateParts[0]
+    );
+
+    return newFirstDate - newSecondDate;
+  });
+};
+
+const setNoteHeaderClassName = (notePriority) => {
+  const basicClassName = "accordion-button collapsed";
+  const priorityClassNames = ["low", "medium", "high"];
+
+  return `${basicClassName} priority-${priorityClassNames[notePriority - 1]}`;
+};
+
+const displayNoteDate = (date) => {
+  const dateParts = date.split("/");
+  const day = dateParts[0] < 9 ? `0${dateParts[0]}` : dateParts[0];
+  const month = dateParts[1] < 9 ? `0${dateParts[1]}` : dateParts[1];
+  const year = dateParts[2];
+
+  return `${day}/${month}/${year}`;
 };
 
 onMounted(() => {
@@ -64,7 +106,7 @@ onMounted(() => {
           />
         </div>
         <div class="col-10 col-xxl-6">
-          <h3 class="mt-10 mb-7">
+          <h3 class="mb-7">
             {{ $t("calendar.addedNotesSectionHeader") }}
           </h3>
           <p v-if="!savedNotes.length">
@@ -78,16 +120,20 @@ onMounted(() => {
             >
               <div class="accordion-header d-flex" :id="`note${index}`">
                 <button
-                  class="accordion-button collapsed"
+                  :class="setNoteHeaderClassName(note.priority)"
                   type="button"
                   data-bs-toggle="collapse"
                   :data-bs-target="`#note${index}Collapse`"
                   aria-expanded="true"
                   :aria-controls="`note${index}-collapse${index}`"
                 >
-                  <span>
-                    <strong>{{ note.date }}</strong> {{ note.title }}
-                  </span>
+                  <span class="accordion-header__date"
+                    >{{ $t("calendar.noteDate") }}
+                    {{ displayNoteDate(note.date) }}</span
+                  >
+                  <span class="accordion-header__title"
+                    >{{ $t("calendar.noteTitle") }} {{ note.title }}</span
+                  >
                 </button>
               </div>
               <div
@@ -117,4 +163,37 @@ onMounted(() => {
   <ErrorView v-else />
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.accordion {
+  &-header__date {
+    min-width: 150px;
+  }
+
+  &-button:not(.collapsed),
+  &-button:not(.collapsed) span {
+    color: black;
+  }
+}
+
+.priority {
+  box-shadow: none;
+
+  &-high {
+    background: #f1aeb5;
+  }
+
+  &-medium {
+    background: #f7caa0;
+  }
+
+  &-low {
+    background: #a3cfbb;
+  }
+}
+
+#notes {
+  .accordion-item {
+    margin-bottom: 12px;
+  }
+}
+</style>
