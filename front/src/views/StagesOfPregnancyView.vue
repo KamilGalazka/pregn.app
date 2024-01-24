c286dc3ac90fd0de236adf45f1dd9947c7dbaff0
Kamil Galazka <kamilg332@gmail.com>
2024-01-24 02:56:39 +0100

    layout adjustment in stages page

--- front/src/views/StagesOfPregnancyView.vue
+++ front/src/views/StagesOfPregnancyView.vue
@@ -96,7 +96,8 @@ onMounted(() => {
         {{ $t("stages.trimester") }} {{ trimester }}
       </div>
     </div>
-    <div class="map__weeks d-flex flex-wrap">
+    <div class="map__weeks d-flex flex-wrap justify-content-between">
+      <div class="map__weeks--line"></div>
       <div
         v-for="element in weeksArray"
         :key="element"
@@ -109,74 +110,77 @@ onMounted(() => {
       </div>
     </div>
   </div>
-  <div
-    class="stages__information-container d-flex flex-column align-items-center col-xxl-8 mx-auto"
-  >
-    <div class="stage__information d-flex flex-column">
-      <div>
-        <h3>{{ $t("stages.week") }} {{ chosenWeek }}</h3>
-      </div>
-      <p class="h3 text-success text-center">
-        {{ $t("stages.informationSectionHeader") }}
-      </p>
-      <p>
-        {{
-          currentLanguage === "pl"
-            ? fetalInformation.description_pl
-            : fetalInformation.description_en
-        }}
-      </p>
-    </div>
-    <div class="d-flex flex-column align-items-center gap-4">
-      <p
-        class="h3 text-success text-center"
-        v-if="chosenWeek >= firstComparisonToFruitWeek"
-      >
-        {{ $t("stages.comparisonSectionHeader") }}
-      </p>
-      <span
-        class="h6"
-        v-if="currentLanguage === 'pl' && fetalInformation.image_header_pl"
-      >
-        {{ fetalInformation.image_header_pl }}
-      </span>
-      <span
-        class="h6"
-        v-else-if="currentLanguage === 'en' && fetalInformation.image_header_en"
-      >
-        {{ fetalInformation.image_header_en }}
-      </span>
-      <img
-        class="stage__information--fruit-image"
-        :src="getStageImage(chosenWeek)"
-        alt="Current week"
-      />
-    </div>
+  <div class="container-lg m-auto">
     <div
-      class="stage__information--numbers d-flex flex-column align-items-center gap-4"
-      v-if="chosenWeek >= firstComparisonInNumbersWeek"
+      class="stages__information-container m-auto row d-flex justify-content-center flex-md-row flex-column align-items-center align-items-lg-start"
     >
-      <p class="h3 text-success text-center">
-        {{ $t("stages.comparisonInNumbersSectionHeader") }}
-      </p>
-      <div class="numbers__container d-flex gap-5">
-        <div class="size d-flex flex-column justify-content-between">
-          <span class="text-uppercase">{{
-            $t("stages.comparisonInNumbersSectionSize")
-          }}</span>
-          <span
-            ><strong>{{ fetalInformation.length_cm }}</strong> cm</span
+      <div class="stage__information col-12 col-lg-6 mb-5">
+        <p class="h3 text-success text-center mb-6">
+          {{ $t("stages.informationSectionHeader") }}
+        </p>
+        <p class="text-center">
+          {{
+            currentLanguage === "pl"
+              ? fetalInformation.description_pl
+              : fetalInformation.description_en
+          }}
+        </p>
+      </div>
+      <div class="col-12 col-lg-6 d-flex flex-column gap-5">
+        <div class="d-flex flex-column align-items-center gap-4">
+          <p
+            class="h3 text-success text-center"
+            v-if="chosenWeek >= firstComparisonToFruitWeek"
           >
-          <img src="../assets/ruler.svg" />
+            {{ $t("stages.comparisonSectionHeader") }}
+          </p>
+          <span
+            class="h6 text-center"
+            v-if="currentLanguage === 'pl' && fetalInformation.image_header_pl"
+          >
+            {{ fetalInformation.image_header_pl }}
+          </span>
+          <span
+            class="h6"
+            v-else-if="
+              currentLanguage === 'en' && fetalInformation.image_header_en
+            "
+          >
+            {{ fetalInformation.image_header_en }}
+          </span>
+          <img
+            class="stage__information--fruit-image"
+            :src="getStageImage(chosenWeek)"
+            alt="Current week"
+          />
         </div>
-        <div class="weight d-flex flex-column justify-content-between">
-          <span class="text-uppercase">{{
-            $t("stages.comparisonInNumbersSectionWeight")
-          }}</span>
-          <span
-            ><strong>{{ fetalInformation.weight_kg }}</strong> kg</span
-          >
-          <img src="../assets/weight-scale.svg" />
+        <div
+          class="stage__information--numbers d-flex flex-column align-items-center gap-4"
+          v-if="chosenWeek >= firstComparisonInNumbersWeek"
+        >
+          <p class="h3 text-success text-center">
+            {{ $t("stages.comparisonInNumbersSectionHeader") }}
+          </p>
+          <div class="numbers__container d-flex gap-5">
+            <div class="size d-flex flex-column justify-content-between">
+              <span class="text-uppercase text-center">{{
+                $t("stages.comparisonInNumbersSectionSize")
+              }}</span>
+              <span class="text-center"
+                ><strong>{{ fetalInformation.length_cm }}</strong> cm</span
+              >
+              <img src="../assets/ruler.svg" />
+            </div>
+            <div class="weight d-flex flex-column justify-content-between">
+              <span class="text-uppercase text-center">{{
+                $t("stages.comparisonInNumbersSectionWeight")
+              }}</span>
+              <span class="text-center"
+                ><strong>{{ fetalInformation.weight_kg }}</strong> kg</span
+              >
+              <img src="../assets/weight-scale.svg" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -186,13 +190,9 @@ onMounted(() => {
 <style scoped lang="scss">
 .stages {
   &__information-container {
-    gap: 40px;
-    margin-top: 24px;
-    margin-bottom: 24px;
+    max-width: 900px;
 
     .stage__information {
-      gap: 24px;
-
       &--fruit-image {
         width: 150px;
         height: 150px;
@@ -209,6 +209,7 @@ onMounted(() => {
 
   &__map-container {
     margin-top: 56px;
+    margin-bottom: 56px;
     gap: 36px;
 
     .map__stages {
@@ -225,26 +226,29 @@ onMounted(() => {
     }
 
     .map__weeks {
-      gap: 5px;
+      max-width: 900px;
+      width: 100%;
+      position: relative;
 
       &--week {
         width: 40px;
-        height: 30px;
+        height: 40px;
         border-radius: 50px;
         cursor: pointer;
         text-align: center;
+        background: $basic-color-blue;
+        color: white;
 
         &:hover {
-          background: antiquewhite;
+          background: darken($basic-color-blue, 20);
 
           span {
             font-size: 18px;
-            background: antiquewhite;
           }
         }
 
         span {
-          line-height: 30px;
+          line-height: 40px;
         }
 
         &.current {
@@ -254,6 +258,16 @@ onMounted(() => {
             background: #eeaf56;
           }
         }
+      }
+
+      &--line {
+        position: absolute;
+        top: 19px;
+        left: 0;
+        height: 2px;
+        width: calc(100% - 2px);
+        z-index: -1;
+        background-color: black;
       }
     }
   }
